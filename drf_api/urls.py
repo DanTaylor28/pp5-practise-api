976031deb29ad01e6e7ec1bb5640c1fa9f1e4b19
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-08 15:32:47 +0000

    Create home-route for landing page

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home_route
 
 urlpatterns = [
+    path('', home_route),
     path('admin/', admin.site.urls),
 ]
