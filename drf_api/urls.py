8b1730351be951447e2ab38b109562eccafe893e
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-09 14:46:00 +0000

    Add ability to log in and out via api

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -20,6 +20,7 @@ from .views import home_route
 urlpatterns = [
     path('', home_route),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
 ]
