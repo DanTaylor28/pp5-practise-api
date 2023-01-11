a942439c29d99232b498bf014b2723370a632839
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-11 19:34:24 +0000

    Define get_post_owner and display it on api

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,14 +3,19 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    # owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source='owner.username')
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    is_post_owner = serializers.SerializerMethodField()
+
+    def get_is_post_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'title', 'caption',
+            'id', 'title', 'caption', 'owner', 'is_post_owner',
             'uploaded_at', 'updated_at', 'image',
             'profile_id', 'profile_image'
         ]
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,9 +5,9 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     # Enables owner field to display the username rather than a number
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_post_owner = serializers.SerializerMethodField()
+    is_profile_owner = serializers.SerializerMethodField()
 
-    def get_is_post_owner(self, obj):
+    def get_is_profile_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
@@ -15,6 +15,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'name', 'location', 'bio', 'owner',
-            'is_post_owner', 'created_at', 'updated_at',
+            'is_profile_owner', 'created_at', 'updated_at',
             'profile_image'
         ]
