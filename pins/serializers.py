421a02b5833cb81dc140ed8abd503ab8c9d8783b
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-13 13:20:22 +0000

    Pin functionality complete!

--- pins/serializers.py
+++ pins/serializers.py
@@ -4,6 +4,7 @@ from django.db import IntegrityError
 
 
 class PinSerializer(serializers.ModelSerializer):
+    # remember to add docstrings for your classes!
     # displays owners name rather than an integer and automatically fills
     # owner field for us on form to like posts
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -15,10 +16,12 @@ class PinSerializer(serializers.ModelSerializer):
             'created_at'
         ]
 
+    # used to handle duplicate like attempts - has to be defined as create
+    # with self and validated_data as arguments.
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
-                'detaail': 'possible duplicate'
+                'error': 'you cannot like a post more than once'
             })
