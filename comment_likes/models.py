b9ddb5cd8dab3180d8a0e248711a9c0ae691b136
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-16 10:36:11 +0000

    Create commentlike model

---
+++ comment_likes/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from comments.models import Comment
+
+
+class CommentLike(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name='comment_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        # ensures users cannot like the same comment twice
+        unique_together = ['owner', 'comment']
+
+    def __str__(self):
+        return f"{self.comment} liked by {self.owner}"
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'pins',
     'comments',
     'followers',
+    'comment_likes',
     'rest_framework',
 ]
 
